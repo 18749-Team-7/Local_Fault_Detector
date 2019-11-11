f7837b1002879b3116b5925e117c551094279168
Supradeep T R <tr.supradeep@gmail.com>
2019-11-11 18:09:59 -0500

    Add colors palette

--- LFD.py
+++ LFD.py
@@ -7,6 +7,16 @@ import time
 import socket
 import argparse
 
+BLACK =     "\u001b[30m"
+RED =       "\u001b[31m"
+GREEN =     "\u001b[32m"
+YELLOW =    "\u001b[33m"
+BLUE =      "\u001b[34m"
+MAGENTA =   "\u001b[35m"
+CYAN =      "\u001b[36m"
+WHITE =     "\u001b[37m"
+RESET =     "\u001b[0m"
+
 class LocalFaultDetector:
 
     def __init__(self, gfd_address, gfd_hb_interval=1, lfd_port=10000):
